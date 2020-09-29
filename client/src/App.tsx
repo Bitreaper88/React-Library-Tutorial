f99201eff85dd6987c212000056b1b180e8a50f3
borb <perttu.tuovinen@buutti.com>
2020-09-29 18:07:35 +0300

    removed client solutions ;)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom';
-import { Login } from './Login';
 import { LoginFn } from './AuthContext';
-import { Signup } from './Signup';
 import { IUser } from '../../server/src/types';
-import { Search } from "./Search";
-import { MyPage } from "./MyPage";
 
 interface IApp {
     userIsAuthenticated: boolean
@@ -41,53 +37,7 @@ const App: React.FC<IApp> = (props) => {
     return (
         <div>
             <Router>
-                <div className="App">
-                    <header>
-                        <Link to="/"> Home </Link>
-                        <Link to="/searchpage"> Search </Link>
-                        {
-                            userIsAuthenticated ?
-                            <Link to="/mypage"> My page </Link> :
-                            <a href="/#" onClick={setModal("signup")}> Signup </a>
-                        }
-                        {
-                            userIsAuthenticated ?
-                            <a href="/#" onClick={onLogoutClick}> Logout </a> :
-                            <a href="/#" onClick={setModal("login")}> Login </a>
-                        }
-                    </header>
-                    <nav>
-                    </nav>
-                    <main>
-                        <Route exact path="/" render={()=>(
-                            <div>
-                                LIBRARBY
-                            </div>
-                        )}/>
-                        <Route exact path="/mypage" render={()=><MyPage user={user}/>}/>
-                        <Route exact path="/signupsuccessful" render={()=>(
-                            <div>
-                                Signed up successfully!
-                            </div>
-                        )}/>
-                        <Route exact path="/searchpage" render={()=><Search/>}/>
-                    </main>
-                    <Login
-                        login={login}
-                        setToken={setToken}
-                        visible={visibleModal === "login" ? true : false}
-                        setVisibleModal={setVisibleModal}
-                        setRedirectToPage={setRedirectToPage}
-                    />
-                    <Signup
-                        visible={visibleModal === "signup" ? true : false}
-                        setVisibleModal={setVisibleModal}
-                        setRedirectToPage={setRedirectToPage}
-                    />
-                    <footer>
-                    </footer>
-                    {redirectToPage && <Redirect to={redirectToPage}/>}
-                </div>
+                hello!
             </Router>
         </div>
     ); 
