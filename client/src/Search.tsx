f8ddb6802b2a4be7dfa517f76267fb72747be4a8
IMF <ipmafr@gmail.com>
2020-10-20 09:41:25 +0300

    file added

---
+++ client/src/Search.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { API_URL } from './constants';
+
+function Search() {
+    async function searchForString(searchTerm: string) {
+        
+    }
+}
+
+export default Search;
